1179167016428e0a1b03e4d5c8d533a17f23f3a7
belenPadilla <padillabelenrosario@gmail.com>
2023-01-16 11:45:40 -0300

    cambios realizados

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/Navbar'; 
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import CartWidget from './components/CartWidget';
+import CartWidget from './components/CartWidget/index';
+import TarjetasProducto  from './components/Tarjetas/tarjetasproducto';
+import arde from '../src/components/fotosropa/arde.jpg'
+
+
 function App() {
   return (
-    <>
-  <NavBar/>
-  <ItemListContainer></ItemListContainer>
-  </>
+  <div>
+      <NavBar />
+      <ItemListContainer></ItemListContainer>
+    
+      <br></br>
+    <div className='OrdenTarjetas'>
+    <TarjetasProducto 
+    titulo=""
+    precio={500}
+    detalle=""
+    img={arde}
+    ></TarjetasProducto>
+        <TarjetasProducto
+    titulo=""
+    precio={500}
+    detalle=""
+    img="./MAMBO/mambo-proyecto/public/fotosropa/arde.jpg"
+    ></TarjetasProducto>
+        <TarjetasProducto
+    titulo=""
+    precio={500}
+    detalle=""
+    img="./MAMBO/mambo-proyecto/public/fotosropa/arde.jpg"
+    ></TarjetasProducto>
+            <TarjetasProducto
+    titulo=""
+    precio={500}
+    detalle=""
+    img="./MAMBO/mambo-proyecto/public/fotosropa/arde.jpg"
+    ></TarjetasProducto>
+            <TarjetasProducto
+    titulo=""
+    precio={500}
+    detalle=""
+    img="./MAMBO/mambo-proyecto/public/fotosropa/arde.jpg"
+    ></TarjetasProducto>
+
+</div>
+  </div>
   );
-}
+  }
 
 export default App;
---
+++ src/components/Boton/Boton.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+function BotonDiseño () {
+    return (
+        <button type="button" className="btn btn-dark">Agregar al carrito</button>
+        )
+}
+
+export default BotonDiseño;
