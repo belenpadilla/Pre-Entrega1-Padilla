9073d05bb9624ab284dcfbae9533859724d1cfaa
belenPadilla <padillabelenrosario@gmail.com>
2023-01-17 18:32:44 -0300

    empezar de nuevo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import CartWidget from './components/CartWidget/index';
-import arde from '../src/components/fotosropa/arde.jpg';
-import relojesss from '../src/components/fotosropa/relojesss.jpg'
-import riñooo from '../src/components/fotosropa/riñooo.jpg'
 import './App.css';
 import NavBar from './components/NavBar/Navbar'; 
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import {BrowserRouter, Routes,Route} from "react-router-dom";
-import TarjetasProducto from './components/Tarjetas/tarjetasproducto';
+
 function App() {
   return (
     <BrowserRouter>
@@ -18,12 +15,11 @@ function App() {
     </Routes>
 
   <NavBar />
-    <ItemListContainer></ItemListContainer>
-    <ItemListContainer></ItemListContainer>
+
       <br></br>
       </div>
       <footer>
-        <p >MAMBO tienda | since 2022 </p>
+        <p className='footer' >MAMBO tienda | since 2022 </p>
       </footer>
       </BrowserRouter>
       
---
+++ src/data/articulos.js
@@ -0,0 +1,29 @@
+
+const articulos = [
+    {
+    titulo:"Top MAMBO",
+    precio: 4500,
+    detalle:"Calidad premiun",
+    img:"/assets/fotosropa/mambo1.jpg",
+    },
+    {
+    titulo:"Riñoneras SOUL",
+    precio: 2000,
+    detalle: "comodidad asegurada",
+    img:"/assets/fotosropa/riñooo.jpg",
+    },
+    {
+    titulo:"Relojes",
+    precio:3900,
+    detalle:"relojes",
+    img:"/assets/fotosropa/relojesss.jpg",
+    },
+    {
+    titulo:"Conjuntos",
+    precio:7900,
+    detalle:"conjunto limitado",
+    img:"/assets/fotosropa/arde.jpg",
+    }
+];
+
+export default articulos;
